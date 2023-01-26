bc2c26ed6a19e92258c8ca599b1b2884f4f31356
Marcos Paulo Moreti <mmoreti@hia.ai>
2023-01-26 18:40:40 -0300

    - Changed CustomMaterialLoader to use methods from 3js instead of creating our own, allowing support of all types of 3js materials.

--- examples/jsm/loaders/CustomMaterialLoader.js
+++ examples/jsm/loaders/CustomMaterialLoader.js
@@ -17,133 +17,46 @@ export class CustomMaterialLoader {
     return data;
   }
 
-  /*
-      Code based on Three.js MaterialLoader
-  */
   async applyProperties(mesh, materialProperties) {
     let material = mesh.material;
     let json = materialProperties;
 
     let objectLoader = new THREE.ObjectLoader();
 
-    let textures = null;
+    let textures = {};
     if (json.images != null && json.images.length > 0) {
       let images = await objectLoader.parseImagesAsync(json.images);
       textures = objectLoader.parseTextures(json.textures, images);
     }
 
-    if (json.color != null && material.color != null) material.color.setHex(json.color);
-    if (json.roughness != null) material.roughness = json.roughness;
-    if (json.metalness != null) material.metalness = json.metalness;
-    if (json.emissive != null && material.emissive != null) material.emissive.setHex(json.emissive);
-    if (json.fog != null) material.fog = json.fog;
-    if (json.flatShading != null) material.flatShading = json.flatShading;
-    if (json.blending != null) material.blending = json.blending;
-    if (json.side != null) material.side = json.side;
-    if (json.shadowSide != null) material.shadowSide = json.shadowSide;
-    if (json.opacity != null) material.opacity = json.opacity;
+    let materialLoader = new THREE.MaterialLoader();
+    materialLoader.setTextures(textures);
+    let mat = materialLoader.parse(materialProperties);
 
-    if (json.transparent != null) material.transparent = json.transparent;
-    else material.transparent = false;
+    let isMapValid = (map) => {
+      return map != null && map.image != null;
+    };
 
-    if (json.alphaTest != null) material.alphaTest = json.alphaTest;
-    if (json.depthTest != null) material.depthTest = json.depthTest;
-    if (json.depthWrite != null) material.depthWrite = json.depthWrite;
-    if (json.colorWrite != null) material.colorWrite = json.colorWrite;
+    if (isMapValid(mesh.material.map) && !isMapValid(mat.map)) {
+      mat.map = mesh.material.map;
 
-    if (json.stencilWrite != null) material.stencilWrite = json.stencilWrite;
-    if (json.stencilWriteMask != null) material.stencilWriteMask = json.stencilWriteMask;
-    if (json.stencilFunc != null) material.stencilFunc = json.stencilFunc;
-    if (json.stencilRef != null) material.stencilRef = json.stencilRef;
-    if (json.stencilFuncMask != null) material.stencilFuncMask = json.stencilFuncMask;
-    if (json.stencilFail != null) material.stencilFail = json.stencilFail;
-    if (json.stencilZFail != null) material.stencilZFail = json.stencilZFail;
-    if (json.stencilZPass != null) material.stencilZPass = json.stencilZPass;
-
-    if (json.wireframe != null) material.wireframe = json.wireframe;
-    if (json.wireframeLinewidth != null) material.wireframeLinewidth = json.wireframeLinewidth;
-    if (json.wireframeLinecap != null) material.wireframeLinecap = json.wireframeLinecap;
-    if (json.wireframeLinejoin != null) material.wireframeLinejoin = json.wireframeLinejoin;
-
-    if (json.polygonOffset != null) material.polygonOffset = json.polygonOffset;
-    if (json.polygonOffsetFactor != null) material.polygonOffsetFactor = json.polygonOffsetFactor;
-    if (json.polygonOffsetUnits != null) material.polygonOffsetUnits = json.polygonOffsetUnits;
-
-    if (json.dithering != null) material.dithering = json.dithering;
-
-    if (json.alphaToCoverage != null) material.alphaToCoverage = json.alphaToCoverage;
-    if (json.premultipliedAlpha != null) material.premultipliedAlpha = json.premultipliedAlpha;
-
-    if (json.visible != null) material.visible = json.visible;
-
-    if (json.toneMapped != null) material.toneMapped = json.toneMapped;
-
-    if (json.userData != null) material.userData = json.userData;
-
-    if (json.vertexColors != null) {
-      if (typeof json.vertexColors === "number") {
-        material.vertexColors = json.vertexColors > 0 ? true : false;
-      } else {
-        material.vertexColors = json.vertexColors;
-      }
-    }
-
-    // Deprecated
-    if (json.shading != null) material.flatShading = json.shading === 1; // THREE.FlatShading
-
-    if (json.map != null && json.textures != null && material.map != null) {
       let textureProperties = json.textures.find(t => t.uuid == json.map);
       if (textureProperties) {
         material.map.format = textureProperties.format;
         material.map.needsUpdate = true;
       }
     }
-
-    if (json.alphaMap != null && textures != null) material.alphaMap = textures[json.alphaMap];
-
-    if (json.bumpMap != null && textures != null) material.bumpMap = textures[json.bumpMap];
-    if (json.bumpScale != null) material.bumpScale = json.bumpScale;
-
-    if (json.normalMapType != null) material.normalMapType = json.normalMapType;
-    if (json.normalScale != null) {
-      let normalScale = json.normalScale;
-
-      if (Array.isArray(normalScale) === false) {
-        // Blender exporter used to export a scalar. See #7459
-        normalScale = [normalScale, normalScale];
-      }
-
-      material.normalScale = new THREE.Vector2().fromArray(normalScale);
+    if (isMapValid(mesh.material.emissiveMap) && !isMapValid(mat.emissiveMap)) {
+      mat.emissiveMap = mesh.material.emissiveMap;
+    }
+    if (isMapValid(mesh.material.normalMap) && !isMapValid(mat.normalMap)) {
+     mat.normalMap = mesh.material.normalMap;
+    }
+    if (isMapValid(mesh.material.aoMap) && !isMapValid(mat.aoMap)) {
+      mat.aoMap = mesh.material.aoMap;
     }
 
-    if (json.displacementMap != null && textures != null)
-      material.displacementMap = textures[json.displacementMap];
-    if (json.displacementScale != null) material.displacementScale = json.displacementScale;
-    if (json.displacementBias != null) material.displacementBias = json.displacementBias;
-
-    if (json.roughnessMap != null && textures != null)
-      material.roughnessMap = textures[json.roughnessMap];
-    if (json.metalnessMap != null && textures != null)
-      material.metalnessMap = textures[json.metalnessMap];
-
-    if (json.emissiveIntensity != null) material.emissiveIntensity = json.emissiveIntensity;
-
-    if (json.envMap != null && textures != null) material.envMap = textures[json.envMap];
-    if (json.envMapIntensity != null) material.envMapIntensity = json.envMapIntensity;
-    if (json.refractionRatio != null) material.refractionRatio = json.refractionRatio;
-
-    if (json.lightMap != null && textures != null) material.lightMap = textures[json.lightMap];
-    if (json.lightMapIntensity != null) material.lightMapIntensity = json.lightMapIntensity;
-
-    if (json.aoMapIntensity != null) material.aoMapIntensity = json.aoMapIntensity;
-
-    if (material.alphaMap != null) material.alphaMap.needsUpdate = true;
-    if (material.bumpMap != null) material.bumpMap.needsUpdate = true;
-    if (material.displacementMap != null) material.displacementMap.needsUpdate = true;
-    if (material.roughnessMap != null) material.roughnessMap.needsUpdate = true;
-    if (material.metalnessMap != null) material.metalnessMap.needsUpdate = true;
-    if (material.envMap != null) material.envMap.needsUpdate = true;
-    if (material.lightMap != null) material.lightMap.needsUpdate = true;
-    material.needsUpdate = true;
+    mesh.material = mat;
+    mesh.material.needsUpdate = true;
   }
 }
